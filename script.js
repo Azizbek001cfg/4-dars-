b89e4281c5b315ff130c265451b444c7b271c5f9
Azizbek <azizbek.aslonov2010@gmail.com>
2024-12-23 18:40:02 +0500

    q

---
+++ script.js
@@ -0,0 +1,47 @@
+// 1-ko'rinish:
+// let money = 30000
+// if(money>=30000) {
+//     console.log("Lavash yeyman");
+// }
+
+// 2-ko'rinish:
+// let money = 25000
+// if(money>=30000) {
+//     console.log("Lavash yeyman");
+// } else {
+//     console.log("Lavashga pulim yetmaydi");
+// }
+
+// let ism = prompt("Ismingizni kiriting:")
+// alert("Salom "+ism);
+
+// let birthDay = prompt("Tug'ilgan yilingizni kiriting:")
+// let yosh = new Date().getFullYear()-birthDay
+// alert("Sizning yoshingiz "+yosh+" da")
+
+// let yosh = prompt("Yoshingizni kiriting:")
+// if(yosh>=16) {
+//     alert("Passport olib bilasiz")
+// } else {
+//     alert("Passport ololmaysiz")
+// }
+
+// let number = prompt("Sonni kiriting:")
+// if(number>10) {
+//     alert("10 dan katta")
+// } else {
+//     alert("10 dan katta emas")
+// }
+
+// 3-ko'rinish:
+// let number = prompt("Sonni kiriting:")
+// if(number>10) {
+//     alert("10 dan katta")
+// } else if(number<10) {
+//     alert("10 dan kichik")
+// } else {
+//     alert("10 ga teng")
+// }
+
+// string tipidan number tipiga o'tib olish: 
+let number1 = parseInt(prompt("Birinchi sonni kiriting:"))
